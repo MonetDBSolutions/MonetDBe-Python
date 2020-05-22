988b556186ebb599d54fd73e5c4efd945895cf44
Gijs Molenaar <gijs@pythonic.nl>
2020-05-22 14:26:20 +0200

    prepare for 0.5 release

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ extras_require = {
 
 setuptools.setup(
     name="monetdbe",
-    version="0.4",
+    version="0.5",
     author="Gijs Molenaar",
     author_email="gijs@pythonic.nl",
     description="MonetDBe - the Python embedded MonetDB",
