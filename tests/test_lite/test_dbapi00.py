2aa79098a58175e6e9d52d94ad9ef229430b55e5
Gijs Molenaar <gijs@pythonic.nl>
2020-05-29 14:13:07 +0200

    more array work

--- tests/test_lite/test_dbapi00.py
+++ tests/test_lite/test_dbapi00.py
@@ -3,12 +3,11 @@ import pandas
 import pytest
 
 
-@pytest.mark.skipif(True, reason="monetdblite compatibl but: not supported yet")
 class TestSimpleDBAPI:
     def test_regular_selection(self, monetdbe_cursor):
         monetdbe_cursor.execute('SELECT * FROM integers')
         result = monetdbe_cursor.fetchall()
-        assert result == [[0], [1], [2], [3], [4], [5], [6], [7], [8], [9], [None]], "Incorrect result returned"
+        assert result == [(0,), (1,), (2,), (3,), (4,), (5,), (6,), (7,), (8,), (9,), (None,)], "Incorrect result returned"
 
     def test_numpy_selection(self, monetdbe_cursor):
         monetdbe_cursor.execute('SELECT * FROM integers')
