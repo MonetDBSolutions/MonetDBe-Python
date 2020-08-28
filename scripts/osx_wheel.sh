fa28f4793d907652d083bdf8602fa7b425797e86
Gijs Molenaar <gijs@pythonic.nl>
2020-08-28 08:35:54 +0200

    small steps

--- scripts/osx_wheel.sh
+++ scripts/osx_wheel.sh
@@ -1,28 +1,30 @@
 #!/usr/bin/env bash
 # Use this to make binary wheels for OSX
-#
+set -e
+set -v
+
+
 # this script uses homebrew, install with:
 #
 #   $ /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 #
 # then install some requirements:
 #
-#   $ brew install cmake bison openssl pyenv readline bzip2
-#
+brew install cmake bison openssl pyenv readline bzip2
 
-set -e
-set -v
 
 # some settings and variables
 PYTHONS=(3.6.10 3.7.7 3.8.3 3.9.0b3) # pypy3.6-7.3.1
 BRANCH=oscar
+
+# no lets set some derivative variables
 HERE="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 BUILD=${HERE}/build
 SRC=${BUILD}/MonetDB-${BRANCH}
 PREFIX=${BUILD}/monetdb
 
 # make sure the pythons are installed
-for p in "${PYTHONS[@]}"; do
+for p in ${PYTHONS[@]}; do
     pyenv install -s ${p}
 done
 
@@ -32,13 +34,14 @@ cd ${BUILD}
 
 # download and install monetdb
 if [ ! -f  ${BRANCH}.tar.bz2 ]; then
-    curl -O -C - https://dev.monetdb.org/hg/MonetDB/archive/${BRANCH}.tar.bz2 
+    curl -O -C - https://dev.monetdb.org/hg/MonetDB/archive/${BRANCH}.tar.bz2
 fi
 
-if [ ! -d ${SRC} ]; then 
-    tar jxvf ${BRANCH}.tar.bz2 
+if [ ! -d ${SRC} ]; then
+    tar jxvf ${BRANCH}.tar.bz2
 fi
 
+# compile the cargo
 mkdir -p ${SRC}/build
 cd ${SRC}/build
 cmake .. -DPY3INTEGRATION=OFF -DBISON_EXECUTABLE=/usr/local/opt/bison/bin/bison -DCMAKE_INSTALL_PREFIX=${PREFIX} -DINT128=OFF -DWITH_CRYPTO=OFF -DCMAKE_BUILD_TYPE=Release -DASSERT=OFF
@@ -47,14 +50,17 @@ make -j5 install
 
 # set some flags to find the monetdb libs
 export CFLAGS="-I${PREFIX}/include -L${PREFIX}/lib"
-export DYLD_LIBRARY_PATH=${PREFIX}/lib 
+export DYLD_LIBRARY_PATH=${PREFIX}/lib
 
+# back to the origin
 cd ${HERE}
 
-for p in "${PYTHONS[@]}"; do
+# make the wheels
+for p in ${PYTHONS[@]}; do
     ~/.pyenv/versions/${p}/bin/pip install wheel
     ~/.pyenv/versions/${p}/bin/python setup.py bdist_wheel
 done
 
+# make the wheels portable
 ~/.pyenv/versions/${PYTHONS[0]}/bin/pip install delocate
 DYLD_LIBRARY_PATH=${PREFIX}/lib/ ~/.pyenv/versions/${PYTHONS[0]}/bin/delocate-wheel -v dist/*.whl
--- scripts/win_wheel.bat
+++ scripts/win_wheel.bat
@@ -1,15 +1,26 @@
 
-:: as adminitrator run
+:: in adminitrator powershell run
+
+:: install chocolatey
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" `
  -NoProfile -InputFormat None -ExecutionPolicy Bypass `
  -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" `
   && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 choco feature enable -n allowGlobalConfirmation
+
+:: install deps
 choco install winflexbison git
+choco install python --version 3.6 --force
+choco install python --version 3.7 --force
+choco install python --version 3.8 --force
+choco install python --version 3.9 --force
+
+:: install visual studio 2017
 cinst VisualStudio2017community --package-parameters `
   "--add Microsoft.VisualStudio.Workload.NativeDesktop --add microsoft.visualstudio.component.vc.cmake.project --add microsoft.visualstudio.component.vc.ATLMFC"
 refreshenv
-
+"c:\program files (x86)\microsoft visual studio\2017\community\common7\tools\vsdevcmd.bat"
+"c:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
 
 :: install deps
 mkdir c:\build
@@ -17,13 +28,12 @@ cd c:\build
 git clone https://github.com/microsoft/vcpkg
 cd vcpkg
 bootstrap-vcpkg.bat -disableMetrics
-:: vcpkg integrate install
-:: needed for 64 bits (with the available python being 64 bit this is needed)
 set VCPKG_DEFAULT_TRIPLET=x64-windows
-vcpkg install libiconv openssl bzip2 geos libxml2 pcre pcre2 zlib getopt
+vcpkg install libiconv openssl bzip2 geos libxml2 pcre pcre2 zlib getopt --triplet x64-windows
 
 
 :: get monetdb
+cd c:\build
 curl -O -C - https://dev.monetdb.org/hg/MonetDB/archive/oscar.tar.bz2
 tar jxvf oscar.tar.bz2
 
@@ -41,13 +51,19 @@ cmake -G "Visual Studio 15 2017" `
 cmake --build . --target ALL_BUILD --parallel 16
 cmake --build . --target INSTALL
 
+cd c\build\Monetdbe-python
+cp c:\build\vcpkg\installed\x64-windows\bin\*.dll monetdbe\.
+cp C:\build\monetdb-installed\bin\*.dll monetdbe\.
+
 
 :: build binary python stuff
-cd c:\build\monetdbe-Python
-python setup.py build_ext `
+C:\Python36\python setup.py build_ext `
   --include-dirs=C:\build\monetdb-installed\include `
-  --library-dirs=C:\build\monetdb-installed\bin
+  --library-dirs=C:\build\monetdb-installed\lib
 
 
 :: make wheel
-python setup.py bdist_wheel
+C:\Python36\python setup.py bdist_wheel
+
+
+:: and repeat for each python (3.6, 3.7, 3.8, 3.9)
