73173d689504017d119b18b901f959630807cd23
Gijs Molenaar <gijs@pythonic.nl>
2020-08-31 09:01:28 +0200

    small steps

--- scripts/win_wheel.ps1
+++ scripts/win_wheel.ps1
@@ -15,8 +15,9 @@ $BRANCH = 'oscar'
 
 # get monetdb
 cd c:\
-curl -O -C - https://dev.monetdb.org/hg/MonetDB/archive/$BRANCH.tar.bz2
-tar jxvf $BRANCH.tar.bz2
+curl -O - https://dev.monetdb.org/hg/MonetDB/archive/$BRANCH.tar.bz2
+bzip2 -d $BRANCH.tar.bz2
+tar -xvf $BRANCH.tar
 
 
 # compile monetdb
