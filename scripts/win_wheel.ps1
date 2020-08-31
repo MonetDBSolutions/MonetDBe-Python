1cf0a7f1c473dbf06ba14e7adbfa448432aa22b9
Gijs Molenaar <gijs@pythonic.nl>
2020-08-31 10:38:31 +0200

    improving windows build

--- scripts/win_wheel.ps1
+++ scripts/win_wheel.ps1
@@ -1,26 +1,28 @@
+$BRANCH = 'oscar'
+
 # install choco deps
-choco install winflexbison git
+#choco install winflexbison git
 #choco install python --version 3.6 --force
 #choco install python --version 3.7 --force
-choco install python --version 3.8 --force
+#choco install python --version 3.8 --force
 #choco install python --version 3.9 --force
 
 # install deps
-cd C:\vcpkg
+#cd C:\vcpkg
 #bootstrap-vcpkg.bat -disableMetrics
-vcpkg install libiconv openssl bzip2 geos libxml2 pcre pcre2 zlib getopt --triplet x64-windows
+#vcpkg install libiconv openssl bzip2 geos libxml2 pcre pcre2 zlib getopt --triplet x64-windows
 
 
 # get monetdb
 cd c:\
-curl -O -C - https://dev.monetdb.org/hg/MonetDB/archive/oscar.tar.bz2
-tar jxvf oscar.tar.bz2
+curl -O -C - https://dev.monetdb.org/hg/MonetDB/archive/$BRANCH.tar.bz2
+tar jxvf $BRANCH.tar.bz2
 
 
 # compile monetdb
-cd c:\MonetDB-oscar
+cd c:\MonetDB-$BRANCH
 cmake -G "Visual Studio 16 2019" `
-  -DCMAKE_TOOLCHAIN_FILE=C:/vcpkg/scripts/buildsystems/vcpkg.cmake `
+  -DCMAKE_TOOLCHAIN_FILE=$VCPKG_ROOT/scripts/buildsystems/vcpkg.cmake `
   -DCMAKE_INSTALL_PREFIX=C:/monetdb -A x64 `
   -DTESTING=OFF `
   -DCMAKE_BUILD_TYPE=Release `
@@ -34,7 +36,7 @@ cmake --build . --target ALL_BUILD --parallel 4
 cmake --build . --target INSTALL
 
 cd $GITHUB_WORKSPACE
-cp c:\vcpkg\installed\x64-windows\bin\*.dll monetdbe\.
+cp $VCPKG_ROOT\installed\x64-windows\bin\*.dll monetdbe\.
 cp C:\monetdb\bin\*.dll monetdbe\.
 
 
