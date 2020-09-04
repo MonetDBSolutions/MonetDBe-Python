3a1844308511c5ba7f5bd27fa875a3efec513ab8
Gijs Molenaar <gijs@pythonic.nl>
2020-09-04 09:21:49 +0200

    small steps

--- scripts/win_wheel.ps1
+++ scripts/win_wheel.ps1
@@ -1,23 +1,20 @@
 $BRANCH = 'Oct2020'
 
+# print the executed commands
 Set-PSDebug -Trace 1
+
 
 # get monetdb
 Set-Location c:\
 curl.exe -O https://dev.monetdb.org/hg/MonetDB/archive/$BRANCH.tar.bz2
-
-ls
-
-bzip2 -tvv $BRANCH.tar.bz2
-bzip2 -d $BRANCH.tar.bz2
-tar -xf $BRANCH.tar
+unzip.exe $BRANCH.tar.bz2
 
 
 # compile monetdb
 Set-Location c:\MonetDB-$BRANCH
 cmake -G "Visual Studio 16 2019" `
-  -DCMAKE_TOOLCHAIN_FILE=$VCPKG_ROOT/scripts/buildsystems/vcpkg.cmake `
-  -DCMAKE_INSTALL_PREFIX=C:/monetdb -A x64 `
+  -DCMAKE_TOOLCHAIN_FILE=$VCPKG_ROOT\scripts\buildsystems\vcpkg.cmake `
+  -DCMAKE_INSTALL_PREFIX=C:\monetdb -A x64 `
   -DTESTING=OFF `
   -DCMAKE_BUILD_TYPE=Release `
   -DASSERT=OFF `
@@ -37,11 +34,14 @@ Copy-Item C:\monetdb\bin\*.dll monetdbe\.
 
 
 # build binary python stuff
-C:\Python36\python.exe setup.py build_ext `
+C:\Python38\python.exe setup.py build_ext `
   --include-dirs=C:\monetdb\include `
   --library-dirs=C:\monetdb\lib
 
+C:\Python38\python.exe -m pip install --upgrade pip
+C:\Python38\python.exe -m pip install pytest
+C:\Python38\python.exe -m pip install -e .
 
 # make wheel
-C:\Python36\python.exe setup.py bdist_wheel
+C:\Python38\python.exe setup.py bdist_wheel
 
