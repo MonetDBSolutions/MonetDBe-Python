4303306d32c8c5a29624262c74401099670fa311
Gijs Molenaar <gijs@pythonic.nl>
2021-12-29 10:34:05 +0200

    cleanup old oct2020 references and remove backward compat code

--- tests/test_lite/test_pylite00.py
+++ tests/test_lite/test_pylite00.py
@@ -1,6 +1,7 @@
 from unittest import TestCase, skip
 
 import numpy
+import numpy as np
 import pandas as pd
 import pytest
 
@@ -12,26 +13,26 @@ class TestmonetdbeBase(TestCase):
         self.con = monetdbe.connect()
 
     def test_regular_selection(self):
-        con = monetdbe.connect()
-        monetdbe.sql('CREATE TABLE pylite00 (i INTEGER)', client=con)
-        con.execute('INSERT INTO pylite00 VALUES (1), (2), (3), (4), (5)')
-        result = con.execute('SELECT * FROM pylite00').fetchdf()
-        assert len(result['i']) == 5, "Incorrect result"
+        with monetdbe.connect() as con:
+            con.execute('CREATE TABLE pylite00 (i INTEGER)')
+            con.execute('INSERT INTO pylite00 VALUES (1), (2), (3), (4), (5)')
+            result = con.execute('SELECT * FROM pylite00').fetchdf()
+            assert len(result['i']) == 5, "Incorrect result"
 
     def test_monetdbe_create(self):
-        cur = monetdbe.create('pylite01', {'i': numpy.arange(1000)})
-        result = monetdbe.sql('select * from pylite01', client=cur.connection)
+        cur = monetdbe.connect().cursor().create('pylite01', {'i': numpy.arange(1000)})
+        result = cur.execute('select * from pylite01').fetchdf()
         assert len(result['i']) == 1000, "Incorrect result"
 
     def test_monetdbe_insert(self):
-        cur = monetdbe.create('pylite02', {'i': numpy.arange(1000)})
+        cur = monetdbe.connect().cursor().create('pylite02', {'i': numpy.arange(1000)})
         cur.insert('pylite02', {'i': numpy.arange(1000)})
         result = cur.execute('select * from pylite02').fetchdf()
         assert len(result['i']) == 2000, "Incorrect result"
 
     def test_monetdbe_create_multiple_columns(self):
         arrays = numpy.arange(100000).reshape((5, 20000))
-        cur = monetdbe.create('pylite03',
+        cur = monetdbe.connect().cursor().create('pylite03',
                               {'i': arrays[0], 'j': arrays[1], 'k': arrays[2], 'l': arrays[3], 'm': arrays[4]})
         result = cur.execute('select * from pylite03').fetchnumpy()
         assert len(result) == 5, "Incorrect amount of columns"
@@ -61,72 +62,71 @@ class TestmonetdbeBase(TestCase):
         cur.create('pylite05', {'i': numpy.arange(1000)})
 
         # check that table was successfully created
-        result = monetdbe.sql('SELECT MIN(i) AS minimum FROM pylite05', client=conn)
+        result =monetdbe.connect().execute('SELECT MIN(i) AS minimum FROM pylite05', client=conn)
         assert result['minimum'][0] == 0, "Incorrect result"
         # attempt to query the table from another client
         with pytest.raises(monetdbe.DatabaseError):
-            monetdbe.sql('SELECT * FROM pylite05', client=conn2)
+           monetdbe.connect().execute('SELECT * FROM pylite05', client=conn2)
 
         # now commit the table
-        monetdbe.sql('COMMIT', client=conn)
+        monetdbe.connect().execute('COMMIT', client=conn)
         # query the table again from the other client, this time it should be there
-        result = monetdbe.sql('SELECT MIN(i) AS minimum FROM pylite05', client=conn2)
+        result =monetdbe.connect().execute('SELECT MIN(i) AS minimum FROM pylite05', client=conn2)
         assert result['minimum'][0] == 0, "Incorrect result"
 
     def test_non_existent_table(self, ):
         # select from non-existent table
         with pytest.raises(monetdbe.DatabaseError):
-            monetdbe.sql('select * from nonexistenttable')
+           monetdbe.connect().execute('select * from nonexistenttable')
 
     def test_invalid_connection_object(self):
         # invalid connection object
         with pytest.raises(TypeError):
-            monetdbe.sql('select * from tables', client=33)
+           monetdbe.connect().execute('select * from tables', client=33)
 
     def test_invalid_colnames(self):
         # invalid colnames
         with pytest.raises(monetdbe.DatabaseError):
-            monetdbe.create('pylite06', {33: []})
+            monetdbe.connect().cursor().create('pylite06', {33: []})
 
     def test_empty_colnames(self):
         # empty colnames
         with pytest.raises(monetdbe.DatabaseError):
-            monetdbe.create('pylite07', {'': []})
+            monetdbe.connect().cursor().create('pylite07', {'': []})
 
     def test_invalid_key_dict(self):
         # dictionary with invalid keys
         d = dict()
         d[33] = 44
         with pytest.raises(monetdbe.ProgrammingError):
-            monetdbe.create('pylite08', d)
+            monetdbe.connect().cursor().create('pylite08', d)
 
     def test_missing_dict_key(self):
         # missing dict key in insert
-        cur = monetdbe.create('pylite09', dict(a=[], b=[], c=[]))
+        cur = monetdbe.connect().cursor().create('pylite09', dict(a=[], b=[], c=[]))
         with pytest.raises(monetdbe.DatabaseError):
             cur.insert('pylite09', dict(a=33, b=44))
 
     def test_bad_column_number(self):
         # too few columns in insert
-        cur = monetdbe.create('pylite10', dict(a=[], b=[], c=[]))
+        cur = monetdbe.connect().cursor().create('pylite10', dict(a=[], b=[], c=[]))
         with pytest.raises(monetdbe.DatabaseError):
             cur.insert('pylite10', {'a': [33], 'b': [44]})
 
     def test_many_sql_statements(self):
         for i in range(5):  # FIXME 1000
-            conn = monetdbe.connect()
-            cur = conn.execute('CREATE TABLE pylite11 (i INTEGER)')
-            cur.insert('pylite11', {'i': numpy.arange(10).astype(numpy.int32)})
-            result = cur.execute('SELECT * FROM pylite11').fetchdf()
-            assert result['i'][0] == 0, "Invalid result"
-            monetdbe.sql('DROP TABLE pylite11', client=conn)
-            monetdbe.sql('ROLLBACK', client=conn)
-            del conn
+            with monetdbe.connect() as conn:
+                cur = conn.execute('CREATE TABLE pylite11 (i INTEGER)')
+                cur.insert('pylite11', {'i': numpy.arange(10).astype(numpy.int32)})
+                result = cur.execute('SELECT * FROM pylite11').fetchdf()
+                assert result['i'][0] == 0, "Invalid result"
+                conn.execute('DROP TABLE pylite11')
+                conn.execute('ROLLBACK')
 
     def test_null_string_insertion_bug(self):
-        con = monetdbe.connect()
-        monetdbe.sql("CREATE TABLE pylite12 (s varchar(2))", client=con)
-        monetdbe.insert('pylite12', {'s': ['a', None]}, client=con)
-        result = monetdbe.sql("SELECT * FROM pylite12", client=con)
-        expected = numpy.ma.masked_array(['a', 'a'], mask=[0, 1])
-        numpy.testing.assert_array_equal(result['s'], expected)
+        with monetdbe.connect() as con:
+            cur = con.execute("CREATE TABLE pylite12 (s varchar(2))")
+            cur.insert('pylite12', {'s': np.array(['a', None])})
+            result = cur.execute("SELECT * FROM pylite12").fetchnumpy()
+            expected = numpy.ma.masked_array(['a', 'a'], mask=[0, 1])
+            numpy.testing.assert_array_equal(result['s'], expected)
