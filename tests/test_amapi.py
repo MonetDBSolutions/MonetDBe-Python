068fadf66ce9b1d8314bd1ca0fe38441cb0380c2
Yunus Koning <aris.koning@monetdbsolutions.com>
2022-02-25 11:09:36 +0100

    Work around for failing test.

--- tests/test_amapi.py
+++ tests/test_amapi.py
@@ -5,10 +5,10 @@ import pymonetdb  # type: ignore
 
 class TestMapi(unittest.TestCase):
     def test_mapi(self):
-        with monetdbe.connect(autocommit=True, database="devdb", host="localhost", port=0) as con:
+        with monetdbe.connect(autocommit=True, host="localhost", port=0) as con:
             con.execute("CREATE TABLE IF NOT EXISTS test (x,y) AS VALUES (10,'foo'), (0, 'bar'), (20, 'baz'), (NULL,NULL);")
 
-            remote_con = pymonetdb.connect(username="monetdb", password="monetdb", hostname="localhost", port=con.get_port(), database="devdb")
+            remote_con = pymonetdb.connect(username="monetdb", password="monetdb", hostname="localhost", port=con.get_port(), database="in-memory")
             cursor = remote_con.cursor()
             cursor.execute("SELECT * FROM test WHERE x > 5 ORDER BY x")
             result = cursor.fetchall()
